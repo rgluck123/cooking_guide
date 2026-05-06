b69eefd63c7ef95ccd9387d652f6d7fcc28f8e23
rgluck123 <rgluck456@gmail.com>
2026-05-06 16:33:57 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
+/* global workbox, importScripts */
 // Service Worker with Workbox injection point
 // Workbox will inject the precache manifest here
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.4.1/workbox-sw.js');
 
 if (workbox) {
   // Precache manifest injected by Workbox injectManifest
-  workbox.precaching.precacheAndRoute([{"revision":"5c1dc8ca11dd7f81fa8f2b6a9cd38405","url":"manifest.json"},{"revision":"df312d880cdf03646af3dcf5aacfea90","url":"index.html"},{"revision":"3b4fcfcf393eca4d264dca4a4663bc37","url":"icons.svg"},{"revision":"7e840862161341271697daa99a40d76b","url":"favicon.svg"},{"revision":"28e106128433182ae28d13ce81330d44","url":"assets/index-BPAhezWZ.css"},{"revision":"3da292880174a1d3cde8ae8e42373ccc","url":"assets/index-BO00mtIl.js"}] || []);
+  workbox.precaching.precacheAndRoute([{"revision":"8c87866c116680004390aad33f7a631a","url":"manifest.json"},{"revision":"8517913d6afdce7916a19d7fe7e0af8e","url":"index.html"},{"revision":"3b4fcfcf393eca4d264dca4a4663bc37","url":"icons.svg"},{"revision":"7e840862161341271697daa99a40d76b","url":"favicon.svg"},{"revision":"58bb58ec938cdd5dd5400489e3d69c98","url":"images/deboning/debone_step_4.png"},{"revision":"8189ef308da0829bd78eace96cff2691","url":"images/deboning/debone_step_3.png"},{"revision":"f813b3d872da631f932015a16a3530b5","url":"images/deboning/debone_step_2.png"},{"revision":"a58e89c1c822b4584b23ea75e03e67d5","url":"images/deboning/debone_step_1.png"},{"revision":"5e665ce672754cd214a5201a5a220709","url":"assets/index-ZGQ431pU.css"},{"revision":"2d2ba1df06c9ecfcf11b099005e8d67b","url":"assets/index-B7u-om6m.js"}] || []);
 
   // Skip waiting on install
   self.addEventListener('install', (event) => {
